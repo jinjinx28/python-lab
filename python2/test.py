392bdb14e969c17ea5ac8cc4cea5d81b17e56013
jinjin <jinjinx28@gmail.com>
2026-06-05 09:47:27 +0900

    2026-06-05 commit

---
+++ python2/test.py
@@ -0,0 +1,15 @@
+# 변수 a, b에 각각 10, 20 할당하여 출력
+a = 10
+b = 20
+
+print(a)
+print(b)
+print(a+b)
+
+# 문자열 변수 정의 및 출력
+str1 = "Hello "
+str2 = "I'm nuri "
+
+print(type(str1), type(str2))
+print ('-' * 50)
+print(str1 * 5)
